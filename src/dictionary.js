9490fca4d4acb1e829ece0223ebcafca71a2557a
warias <warias@MacBook-Pro-de-it-108.local>
2018-03-27 18:54:48 +0200

    adding status speech and prettier doing its business :(

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,77 +1,74 @@
-import storage from "node-persist";
-import logger from "./logger";
+import storage from 'node-persist';
+import logger from './logger';
 
 const dictionary = [
-  {
-    icc: "89551025139002042482",
-    provider: "Vivo Legacy",
-    paymentModel: "Control",
-    msisdn: "5511942502249"
-  },
-  {
-    icc: "89551093144180753659",
-    provider: "Vivo Legacy",
-    paymentModel: "Prepay",
-    msisdn: "5521995148544"
-  },
-  {
-    icc: "89551093244094886072",
-    provider: "Vivo Legacy",
-    paymentModel: "Prepay",
-    msisdn: "5511956102979"
-  },
-  {
-    icc: "89551093144180753691",
-    provider: "Vivo Legacy",
-    paymentModel: "Postpay",
-    msisdn: "5521995382603"
-  },
-  {
-    icc: "8954079144210016210",
-    provider: "Movistar B2C",
-    paymentModel: "Full",
-    msisdn: "541144203936"
-  },
-  {
-    icc: "8954079144256579014",
-    provider: "Movistar B2B",
-    paymentModel: "Control",
-    msisdn: "541165271099"
-  },
-  {
-    icc: "8944110066707387600",
-    provider: "O2",
-    paymentModel: "postpay",
-    msisdn: "447519194626"
-  },
+    {
+        icc: '89551025139002042482',
+        provider: 'Vivo Legacy',
+        paymentModel: 'Control',
+        msisdn: '5511942502249',
+    },
+    {
+        icc: '89551093144180753659',
+        provider: 'Vivo Legacy',
+        paymentModel: 'Prepay',
+        msisdn: '5521995148544',
+    },
+    {
+        icc: '89551093244094886072',
+        provider: 'Vivo Legacy',
+        paymentModel: 'Prepay',
+        msisdn: '5511956102979',
+    },
+    {
+        icc: '89551093144180753691',
+        provider: 'Vivo Legacy',
+        paymentModel: 'Postpay',
+        msisdn: '5521995382603',
+    },
+    {
+        icc: '8954079144210016210',
+        provider: 'Movistar B2C',
+        paymentModel: 'Full',
+        msisdn: '541144203936',
+    },
+    {
+        icc: '8954079144256579014',
+        provider: 'Movistar B2B',
+        paymentModel: 'Control',
+        msisdn: '541165271099',
+    },
+    {
+        icc: '8944110066707387600',
+        provider: 'O2',
+        paymentModel: 'postpay',
+        msisdn: '447519194626',
+    },
 ];
 
 storage.initSync({
-  dir: "dictionary.storage",
-  stringify: JSON.stringify,
-  parse: JSON.parse,
-  encoding: "utf8",
-  logging: false,
-  continuous: true,
-  interval: false,
-  ttl: false
+    dir: 'dictionary.storage',
+    stringify: JSON.stringify,
+    parse: JSON.parse,
+    encoding: 'utf8',
+    logging: false,
+    continuous: true,
+    interval: false,
+    ttl: false,
 });
-storage.setItemSync("dictionary", dictionary);
+storage.setItemSync('dictionary', dictionary);
 
 const Dictionary = dictionary => ({
-  findSim: ({ channel, icc, msisdn }) =>
-    dictionary.find(
-      item =>
-        (icc && item.icc && icc.startsWith(item.icc)) || 
-        (channel != undefined && item.channel === channel) || 
-        (msisdn && item.msisdn === msisdn)
-    ),
-  getData: () => dictionary,
-  getMsisdns: () => dictionary.map(item => item.msisdn)
+    findSim: ({channel, icc, msisdn}) =>
+        dictionary.find(
+            item => (icc && item.icc && icc.startsWith(item.icc)) || (msisdn && item.msisdn === msisdn)
+        ),
+    getData: () => dictionary,
+    getMsisdns: () => dictionary.map(item => item.msisdn),
 });
 
 const getDictionary = () => {
-  return Dictionary(storage.getItemSync("dictionary"));
+    return Dictionary(storage.getItemSync('dictionary'));
 };
 
 export default getDictionary;
--- src/simtron.js
+++ src/simtron.js
@@ -1,232 +1,232 @@
-import { RtmClient, WebClient, RTM_EVENTS, CLIENT_EVENTS } from "@slack/client";
+import {RtmClient, WebClient, RTM_EVENTS, CLIENT_EVENTS} from '@slack/client';
 import {
-  isMessage,
-  isMessageToChannel,
-  isFromUser,
-  isFromAnyUser,
-  messageContainsText,
-  messageContainsAnyText,
-  messageContainsAllTexts,
-  getValueFromMessage
-} from "./utils";
-import parse, { parseMessage, readLine } from "./data-parser";
-import getDictionary from "./dictionary";
-import { enableSim, disableSim, statusSim } from "./actions";
+    isMessage,
+    isMessageToChannel,
+    isFromUser,
+    isFromAnyUser,
+    messageContainsText,
+    messageContainsAnyText,
+    messageContainsAllTexts,
+    getValueFromMessage,
+} from './utils';
+import parse, {parseMessage, readLine} from './data-parser';
+import getDictionary from './dictionary';
+import {enableSim, disableSim, statusSim} from './actions';
 
 const defaultOptions = {
-  messageColor: "#590088",
-  usePictures: false,
-  logger: console,
-  rtmOptions: {}
+    messageColor: '#590088',
+    usePictures: false,
+    logger: console,
+    rtmOptions: {},
 };
 
-const askForSims = [
-  "sims",
-  "numbers",
-  "telephones",
-  "telefonos",
-  "numeros",
-  "sim",
-  "telefono"
-];
+const askForSims = ['sims', 'numbers', 'telephones', 'telefonos', 'numeros', 'sim', 'telefono'];
 
 const simtron = (botToken, options = {}, port) => {
-  let botId;
+    let botId;
 
-  const opt = Object.assign({}, defaultOptions, options);
-  const rtm = new RtmClient(botToken, opt.rtmOptions);
-  const web = new WebClient(botToken);
+    const opt = Object.assign({}, defaultOptions, options);
+    const rtm = new RtmClient(botToken, opt.rtmOptions);
+    const web = new WebClient(botToken);
 
-  const sendEnableSim = ({ event }) => {
-    const dictionary = getDictionary();
-    const msisdn = getValueFromMessage(event, dictionary.getMsisdns());
-    console.log(msisdn);
-    const channel = dictionary.findSim({ msisdn }).channel;
+    const sendEnableSim = ({event}) => {
+        const dictionary = getDictionary();
+        const msisdn = getValueFromMessage(event, dictionary.getMsisdns());
+        console.log(msisdn);
+        const channel = dictionary.findSim({msisdn}).channel;
 
-    opt.logger.info(`Enabling sim channel: ${channel} for number: ${msisdn}`);
-    enableSim({ port, channel });
-    return "";
-  };
-
-  const sendDisableSim = ({ event }) => {
-    const dictionary = getDictionary();
-    const msisdn = getValueFromMessage(event, dictionary.getMsisdns());
-    const channel = dictionary.findSim({ msisdn }).channel;
-
-    opt.logger.info(`Disabling sim channel: ${channel} for number: ${msisdn}`);
-    disableSim({ port, channel });
-    return "";
-  };
-
-  const postMessageToChannels = message => channels =>
-    channels.map(channel => {
-      opt.logger.info(`Posting message to ${channel.name}`, message);
-      web.chat.postMessage(channel.id, "", message);
-    });
-
-  const getGroups = () => web.groups.list().then(res => res.groups);
-  const getChannels = () =>
-    web.channels
-      .list()
-      .then(res => res.channels.filter(channel => channel.is_member));
-
-  const writeMessage = line => {
-    opt.logger.info(line);
-    const text = parseMessage(line);
-    if (text) {
-      const msgOptions = { as_user: true };
-      const message = { ...msgOptions, attachments: [{ title: text }] };
-      getGroups().then(postMessageToChannels(message));
-      getChannels().then(postMessageToChannels(message));
-    }
-  };
-
-  const answerChannel = ({ opt, event, message }) => {
-    const msgOptions = {
-      as_user: true,
-      attachments: [
-        {
-          color: opt.messageColor,
-          title: message
-        }
-      ]
+        opt.logger.info(`Enabling sim channel: ${channel} for number: ${msisdn}`);
+        enableSim({port, channel});
+        return '';
     };
 
-    web.chat.postMessage(event.channel, "", msgOptions);
-    opt.logger.info(`Posting message to ${event.channel}`, msgOptions);
-  };
-
-  const messageUserName = (username, text) =>
-    (username ? `@${username} ` : ` `) + text;
-
-  const speeches = [
-    {
-      condition: ({ opt, event, dictionary }) =>
-        messageContainsAnyText(event, dictionary.getMsisdns()) &&
-        isFromAnyUser(event, opt.admins) &&
-        messageContainsAnyText(event, "enable"),
-
-      action: ({ event, username, dictionary }) => {
-        sendEnableSim({ event });
-        answerChannel({
-          opt,
-          event,
-          message: messageUserName(username, "Enable sim acknowledged!")
-        });
-      }
-    },
-    {
-      condition: ({ opt, event, dictionary }) =>
-        messageContainsAnyText(event, dictionary.getMsisdns()) &&
-        isFromAnyUser(event, opt.admins) &&
-        messageContainsAnyText(event, "disable"),
-      action: ({ event, username, dictionary }) => {
-        sendDisableSim({ event });
-        answerChannel({
-          opt,
-          event,
-          message: messageUserName(username, "Disable sim acknowledged!")
-        });
-      }
-    },
-    {
-      condition: ({ opt, event, dictionary }) =>
-        messageContainsAnyText(event, dictionary.getMsisdns()) &&
-        isFromAnyUser(event, opt.admins),
-      action: ({ event, username, dictionary }) => {
-        answerChannel({
-          opt,
-          event,
-          message: messageUserName(
-            username,
-            "Use 'enable PHONENUMBER' or 'disable PHONENUMBER' "
-          )
-        });
-      }
-    },
-    {
-      condition: ({ opt, event, dictionary }) =>
-        messageContainsAnyText(event, dictionary.getMsisdns()),
-      action: ({ event, username, dictionary }) => {
-        answerChannel({
-          opt,
-          event,
-          message: messageUserName(username, "Admin actions are restricted")
-        });
-      }
-    },
-    {
-      condition: ({ opt, event, dictionary }) => true,
-      action: ({ event, username, dictionary }) => {
-        answerChannel({
-          opt,
-          event,
-          message: messageUserName(
-            username,
-            getDictionaryMessage({ dictionary })
-          )
-        });
-      }
-    }
-  ];
-
-  const getDictionaryMessage = ({ dictionary }) =>
-    `We have the following numbers: \r\n${dictionary
-      .getData()
-      .map(item => item.msisdn + " " + item.provider + " " + item.paymentModel)
-      .join(
-        "\r\n"
-      )} \r\n You can use these phone numbers to login into NOVUM, if you see no OTP SMS is received for a specific number, contact admins (warias or carlosfernandez) for help.`;
-
-  port.on("data", payload => {
-    opt.logger.info(payload);
-    const decodedInput = payload.toString("utf8");
-    const line = readLine(decodedInput, writeMessage);
-  });
-
-  rtm.on(RTM_EVENTS.MESSAGE, event => {
-    if (
-      isMessage(event) &&
-      isMessageToChannel(event) &&
-      !isFromUser(event, botId) &&
-      messageContainsAnyText(event, opt.botIds)
-    ) {
-      web.users.info(event.user).then(response => {
-        const username = response && response.user && response.user.name;
+    const sendDisableSim = ({event}) => {
         const dictionary = getDictionary();
+        const msisdn = getValueFromMessage(event, dictionary.getMsisdns());
+        const channel = dictionary.findSim({msisdn}).channel;
 
-        const speech = speeches.find(item =>
-          item.condition({ opt, event, dictionary })
-        );
-        if (speech && speech.action) {
-          speech.action({ event, username, dictionary });
-        } else {
-          answerChannel({
-            opt,
-            event,
-            message: "Oops, there was an error!"
-          });
+        opt.logger.info(`Disabling sim channel: ${channel} for number: ${msisdn}`);
+        disableSim({port, channel});
+        return '';
+    };
+
+    const sendStatusSim = ({event}) => {
+        const dictionary = getDictionary();
+        const msisdn = getValueFromMessage(event, dictionary.getMsisdns());
+        const channel = dictionary.findSim({msisdn}).channel;
+
+        opt.logger.info(`Getting status for channel: ${channel} and number: ${msisdn}`);
+        statusSim({port, channel});
+        return '';
+    };
+
+    const postMessageToChannels = message => channels =>
+        channels.map(channel => {
+            opt.logger.info(`Posting message to ${channel.name}`, message);
+            web.chat.postMessage(channel.id, '', message);
+        });
+
+    const getGroups = () => web.groups.list().then(res => res.groups);
+    const getChannels = () =>
+        web.channels.list().then(res => res.channels.filter(channel => channel.is_member));
+
+    const writeMessage = line => {
+        opt.logger.info(line);
+        const text = parseMessage(line);
+        if (text) {
+            const msgOptions = {as_user: true};
+            const message = {...msgOptions, attachments: [{title: text}]};
+            getGroups().then(postMessageToChannels(message));
+            getChannels().then(postMessageToChannels(message));
         }
-      });
-    }
-  });
+    };
 
-  rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, rtmStartData => {
-    botId = rtmStartData.self.id;
-    opt.logger.info(
-      `Logged in as ${rtmStartData.self.name} (id: ${botId}) of team ${
-        rtmStartData.team.name
-      }`
-    );
-  });
+    const answerChannel = ({opt, event, message}) => {
+        const msgOptions = {
+            as_user: true,
+            attachments: [
+                {
+                    color: opt.messageColor,
+                    title: message,
+                },
+            ],
+        };
 
-  return {
-    rtm,
-    web,
-    start() {
-      rtm.start();
-    }
-  };
+        web.chat.postMessage(event.channel, '', msgOptions);
+        opt.logger.info(`Posting message to ${event.channel}`, msgOptions);
+    };
+
+    const messageUserName = (username, text) => (username ? `@${username} ` : ` `) + text;
+
+    const speeches = [
+        {
+            condition: ({opt, event, dictionary}) =>
+                messageContainsAnyText(event, dictionary.getMsisdns()) &&
+                isFromAnyUser(event, opt.admins) &&
+                messageContainsAnyText(event, 'enable'),
+
+            action: ({event, username, dictionary}) => {
+                sendEnableSim({event});
+                answerChannel({
+                    opt,
+                    event,
+                    message: messageUserName(username, 'Enable sim acknowledged!'),
+                });
+            },
+        },
+        {
+            condition: ({opt, event, dictionary}) =>
+                messageContainsAnyText(event, dictionary.getMsisdns()) &&
+                isFromAnyUser(event, opt.admins) &&
+                messageContainsAnyText(event, 'disable'),
+            action: ({event, username, dictionary}) => {
+                sendDisableSim({event});
+                answerChannel({
+                    opt,
+                    event,
+                    message: messageUserName(username, 'Disable sim acknowledged!'),
+                });
+            },
+        },
+        {
+            condition: ({opt, event, dictionary}) =>
+                messageContainsAnyText(event, dictionary.getMsisdns()) &&
+                messageContainsAnyText(event, 'status'),
+            action: ({event, username, dictionary}) => {
+                sendStatusSim({event});
+                answerChannel({
+                    opt,
+                    event,
+                    message: messageUserName(username, 'Getting status info ...'),
+                });
+            },
+        },
+        {
+            condition: ({opt, event, dictionary}) =>
+                messageContainsAnyText(event, dictionary.getMsisdns()) && isFromAnyUser(event, opt.admins),
+            action: ({event, username, dictionary}) => {
+                answerChannel({
+                    opt,
+                    event,
+                    message: messageUserName(username, "Use 'enable PHONENUMBER' or 'disable PHONENUMBER' "),
+                });
+            },
+        },
+        {
+            condition: ({opt, event, dictionary}) => messageContainsAnyText(event, dictionary.getMsisdns()),
+            action: ({event, username, dictionary}) => {
+                answerChannel({
+                    opt,
+                    event,
+                    message: messageUserName(username, 'Admin actions are restricted'),
+                });
+            },
+        },
+        {
+            condition: ({opt, event, dictionary}) => true,
+            action: ({event, username, dictionary}) => {
+                answerChannel({
+                    opt,
+                    event,
+                    message: messageUserName(username, getDictionaryMessage({dictionary})),
+                });
+            },
+        },
+    ];
+
+    const getDictionaryMessage = ({dictionary}) =>
+        `We have the following numbers: \r\n${dictionary
+            .getData()
+            .map(item => item.msisdn + ' ' + item.provider + ' ' + item.paymentModel)
+            .join(
+                '\r\n'
+            )} \r\n You can use these phone numbers to login into NOVUM, if you see no OTP SMS is received for a specific number, contact admins (warias or carlosfernandez) for help.`;
+
+    port.on('data', payload => {
+        opt.logger.info(payload);
+        const decodedInput = payload.toString('utf8');
+        const line = readLine(decodedInput, writeMessage);
+    });
+
+    rtm.on(RTM_EVENTS.MESSAGE, event => {
+        if (
+            isMessage(event) &&
+            isMessageToChannel(event) &&
+            !isFromUser(event, botId) &&
+            messageContainsAnyText(event, opt.botIds)
+        ) {
+            web.users.info(event.user).then(response => {
+                const username = response && response.user && response.user.name;
+                const dictionary = getDictionary();
+
+                const speech = speeches.find(item => item.condition({opt, event, dictionary}));
+                if (speech && speech.action) {
+                    speech.action({event, username, dictionary});
+                } else {
+                    answerChannel({
+                        opt,
+                        event,
+                        message: 'Oops, there was an error!',
+                    });
+                }
+            });
+        }
+    });
+
+    rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, rtmStartData => {
+        botId = rtmStartData.self.id;
+        opt.logger.info(
+            `Logged in as ${rtmStartData.self.name} (id: ${botId}) of team ${rtmStartData.team.name}`
+        );
+    });
+
+    return {
+        rtm,
+        web,
+        start() {
+            rtm.start();
+        },
+    };
 };
 
 export default simtron;
